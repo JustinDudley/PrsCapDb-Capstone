0f9dff53aa55a1db69ec09c57b1df1ade2af81dd
Justin Dudley <justindudley99@gmail.com>
2019-09-16 11:21:45 -0400

    Add project files.

---
+++ PrsCapBackendProject/Models/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrsCapBackendProject.Models {
+    public class User {
+
+        //PROPERTIES
+        int Id { get; set; }    // how to auto-generate?
+        [StringLength(30)]
+        string Username { get; set; }
+        [StringLength(30)]
+        string Password { get; set; }
+        [StringLength(30)]
+        string Firstname { get; set; }
+        [StringLength(30)]
+        string Lastname { get; set; }
+        [StringLength(12)]
+        string Phone { get; set; } // specify string length? Do a CATCH in case system throws exception for too many digits entered?
+        [StringLength(255)]
+        string Email { get; set; }
+        bool IsReviewer { get; set; } = false;
+        bool IsAdmin { get; set; } = false;
+
+    }
+}
